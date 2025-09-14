b3cb9a11c4b22b277789106cb0a8825485b46a01
sianubukek <sianubukek@gmail.com>
2025-09-14 21:33:29 +0700

    Update Instructors page and add all project files

--- SKK Landing Page/src/pages/Instructors.tsx
+++ SKK Landing Page/src/pages/Instructors.tsx
@@ -21,9 +21,9 @@ const Instructors: React.FC = () => {
       name: "Heni E. Sulastri",
       rank: "Sabuk Hitam Dan V",
       experience: "30 tahun pengalaman",
-      specialty: "Pendiri SKK & Guru Besar",
-      description: "Pemegang sabuk hitam Dan 3 dengan pengalaman mengajar lebih dari 30 tahun. Spesialis dalam teknik kumite dan kihon dasar.",
-      achievements: ["Juara 1 Asia Pacific 1991"],
+      specialty: "Pendiri SKK",
+      description: "Pemegang sabuk hitam Dan V dengan pengalaman mengajar lebih dari 30 tahun. Spesialis dalam teknik kumite dan kihon dasar.",
+      achievements: ["Juara 1 Asia Pacific 1991", "BINPRES FORKI 1991", "BINPRES BUDOKAI 1991", "Sport Science KONI 1991"],
       image: "/Heni.jpeg"
     }
   ];
